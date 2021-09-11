329db4d9eed06673b9a73ce52be26157903237ea
jubayer05 <Jubayer05>
2021-09-11 23:08:12 +0600

    testing

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,26 +6,70 @@ import db, { auth, provider } from "./firebase";
 function Header() {
   const [users, setUser] = useState(null);
   const [logInfo, setLogInfo] = useState();
+  const [loadData, setLoadData] = useState(false);
+  const [userData, setUserData] = useState(null);
+  const [dataLocalStorage, setDataLocalStorage] = useState(null);
+
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
       auth.currentUser && console.log("ac");
     });
+    setUser({
+      ...users,
+      name: sessionStorage.getItem("userName"),
+      email: sessionStorage.getItem("userEmail"),
+    });
+
+    setDataLocalStorage({
+      dataLocalStorage: localStorage.getItem("refreshToken"),
+    });
   }, []);
+
+  useEffect(() => {
+    db.collection("users")
+      .doc(users?.uid)
+      .get()
+      .then((result) => {
+        // console.log(result.data());
+        const data = result.data()?.logInfo;
+        setUserData({ refreshToken: data });
+      });
+  }, [loadData]);
+
+  // TODO: Work here
+  useEffect(() => {
+    if (dataLocalStorage === userData?.refreshToken) {
+      console.log("We are on the right way");
+    } else {
+      console.log("something went wrong");
+    }
+  }, [userData?.refreshToken]);
+
   const login = () => {
     auth.setPersistence("session").then(() => {
       auth.signInWithPopup(provider).then((result) => {
         let data = result.user;
-        console.log(data.uid);
-        // const info = db.collection("users").doc(data.uid);
-        // info.get().then((doc) => {
-        //   if (doc.exists) {
-        //     console.log(doc.data().logInfo);
-        //     setLogInfo(doc.data().logInfo);
-        //     !logInfo && info.set({ logInfo: true });
-        //   } else {
-        //     info.set({ logInfo: true });
-        //   }
-        // });
+        sessionStorage.setItem("userName", data.displayName);
+        sessionStorage.setItem("userEmail", data.email);
+        localStorage.setItem("refreshToken", data.refreshToken);
+        setUser({
+          ...users,
+          name: data.displayName,
+          email: data.email,
+          uid: data.uid,
+        });
+        setLoadData(true);
+        // console.log(data);
+        const info = db.collection("users").doc(data.uid);
+        info.get().then((doc) => {
+          if (doc.exists) {
+            console.log(doc.data().logInfo);
+            setLogInfo(doc.data().logInfo);
+            !logInfo && info.set({ logInfo: data.refreshToken });
+          } else {
+            info.set({ logInfo: data.refreshToken });
+          }
+        });
       });
     });
   };
@@ -45,9 +89,17 @@ function Header() {
   // };
 
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <p>Current User Name: {users?.name}</p>
       <p>Current User: {users?.email}</p>
-      <button onClick={login}>Log in</button>;
+      <button onClick={login}>Log in</button>
       {/* {logInfo && <button onClick={logout}>login logout of other</button>} */}
     </div>
   );
