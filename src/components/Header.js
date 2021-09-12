1d1bb36c1aaf5cdf7f10c86945b752e496961e0e
sourabh singh <mshrudra@gmail.com>
2021-09-12 13:42:56 +0530

    done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,92 +1,67 @@
 import React, { useEffect, useState } from "react";
-import { useAuthState } from "react-firebase-hooks/auth";
 
 import db, { auth, provider } from "./firebase";
 
 function Header() {
-  const [users, setUser] = useState(null);
-  const [logInfo, setLogInfo] = useState();
+  const [users, setUser] = useState("abc");
+  const [logInfo, setLogInfo] = useState(false);
   const [loadData, setLoadData] = useState(false);
-  const [userData, setUserData] = useState(null);
-  const [dataLocalStorage, setDataLocalStorage] = useState(null);
-
-  useEffect(() => {
-    auth.onAuthStateChanged((user) => {
-      auth.currentUser && console.log("ac");
-    });
-    setUser({
-      ...users,
-      name: sessionStorage.getItem("userName"),
-      email: sessionStorage.getItem("userEmail"),
-    });
-
-    setDataLocalStorage({
-      dataLocalStorage: localStorage.getItem("refreshToken"),
-    });
-  }, []);
 
   useEffect(() => {
     db.collection("users")
-      .doc(users?.uid)
-      .get()
-      .then((result) => {
-        // console.log(result.data());
+      .doc(
+        !sessionStorage.getItem("user") ? users : sessionStorage.getItem("user")
+      )
+      .onSnapshot((result) => {
         const data = result.data()?.logInfo;
-        setUserData({ refreshToken: data });
+        if (sessionStorage.getItem("refreshToken") === data) {
+          setLoadData(true);
+        } else {
+          console.log("not now");
+        }
       });
-  }, [loadData]);
-
-  // TODO: Work here
-  useEffect(() => {
-    if (dataLocalStorage === userData?.refreshToken) {
-      console.log("We are on the right way");
-    } else {
-      console.log("something went wrong");
-    }
-  }, [userData?.refreshToken]);
+  }, [users]);
 
   const login = () => {
     auth.setPersistence("session").then(() => {
       auth.signInWithPopup(provider).then((result) => {
         let data = result.user;
-        sessionStorage.setItem("userName", data.displayName);
-        sessionStorage.setItem("userEmail", data.email);
-        localStorage.setItem("refreshToken", data.refreshToken);
-        setUser({
-          ...users,
-          name: data.displayName,
-          email: data.email,
-          uid: data.uid,
-        });
-        setLoadData(true);
-        // console.log(data);
+
+        sessionStorage.setItem("refreshToken", data.refreshToken);
+        sessionStorage.setItem("user", data.uid);
+        sessionStorage.setItem("name", data.displayName);
+        sessionStorage.setItem("email", data.email);
+        setUser(sessionStorage.getItem("user"));
+
         const info = db.collection("users").doc(data.uid);
         info.get().then((doc) => {
           if (doc.exists) {
-            console.log(doc.data().logInfo);
-            setLogInfo(doc.data().logInfo);
-            !logInfo && info.set({ logInfo: data.refreshToken });
+            const sessionToken = sessionStorage.getItem("refreshToken");
+            if (sessionToken === doc.data().logInfo) {
+              console.log("you are good to go");
+            } else {
+              setLogInfo(true);
+            }
           } else {
-            info.set({ logInfo: data.refreshToken });
+            info.set({
+              name: data.displayName,
+              email: data.email,
+              logInfo: data.refreshToken,
+            });
           }
         });
       });
     });
   };
-
-  // const logout = () => {
-  //   auth.signOut().then(() => {
-  //     setLogInfo(false);
-  //     info.update({ logInfo: false });
-  //   });
-  //   console.log(users.uid);
-  //   const info = db.collection("users").doc(users.uid);
-
-  //   auth
-  //     .setPersistence("session")
-  //     .then(() => auth.signInWithPopup(provider))
-  //     .then(info.update({ logInfo: true }));
-  // };
+  const setUserLogin = () => {
+    db.collection("users")
+      .doc(sessionStorage.getItem("user"))
+      .update({
+        name: sessionStorage.getItem("name"),
+        email: sessionStorage.getItem("email"),
+        logInfo: sessionStorage.getItem("refreshToken"),
+      });
+  };
 
   return (
     <div
@@ -97,10 +72,21 @@ function Header() {
         flexDirection: "column",
       }}
     >
-      <p>Current User Name: {users?.name}</p>
-      <p>Current User: {users?.email}</p>
       <button onClick={login}>Log in</button>
-      {/* {logInfo && <button onClick={logout}>login logout of other</button>} */}
+      {logInfo && !loadData && (
+        <button onClick={setUserLogin}>
+          You are logged in using the same email ID on another
+          tab/browser/device. Do you wish to logout from all others and continue
+          on this tab?
+        </button>
+      )}
+      {loadData && (
+        <>
+          <p>Current User Name: {sessionStorage.getItem("name")}</p>
+          <p>Current User: {sessionStorage.getItem("email")}</p>
+          <p>you are logged in</p>
+        </>
+      )}
     </div>
   );
 }
