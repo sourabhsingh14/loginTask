7e5c4f490ab4bef554c413eee382c534e75c2ac2
sourabh singh <mshrudra@gmail.com>
2021-09-12 15:17:23 +0530

    current change

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from "react";
 
 import "./App.css";
 
-import Header from "./components/Header";
+import Header from "./components/Login";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Login />
     </div>
   );
 }
